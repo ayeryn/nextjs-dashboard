fbad770de65ab70c62b9046a93a12d168b4d6ef1
Eryn Li <erynli@utexas.edu>
2024-09-24 12:12:51 -0500

    revenue chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { lusitana } from "../ui/fonts";
+import { fetchRevenue } from "../lib/data";
+import RevenueChart from "../ui/dashboard/revenue-chart";
 
 export default async function Page() {
   // Page is an async component. This allows you to use await to fetch data.
+
+  const revenue = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:test-2xl`}>
@@ -19,7 +23,7 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
+        <RevenueChart revenue={revenue} />
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
