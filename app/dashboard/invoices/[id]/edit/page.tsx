e242702ef36a701b2904c5ce25f10533944114f7
Eryn Li <erynli@utexas.edu>
2024-09-24 20:21:11 -0500

    read id from params prop

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/edit-form";
 
-const page = () => {
-  return <div>Update Invoice</div>;
-};
-
-export default page;
+export default async function EditInvoice({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const id = params.id;
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      {/* <Form invoice={invoice} customers={customer} /> */}
+    </main>
+  );
+}
