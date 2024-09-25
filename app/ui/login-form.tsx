f61ab757d26d187cb70063a164a2331158236d54
Eryn Li <erynli@utexas.edu>
2024-09-25 15:20:18 -0500

    clean up

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,7 +8,7 @@ import {
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
 import { useActionState } from "react";
-import { authenticate, State } from "../lib/actions";
+import { authenticate } from "../lib/actions";
 
 export default function LoginForm() {
   const [errorMessage, formAction, isPending] = useActionState(
