85c7ba1a2c7c7531fdd92f7df08fd204614bab25
Eryn Li <erynli@utexas.edu>
2024-09-24 19:47:15 -0500

    create form

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,24 @@
+import { fetchCustomers } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/create-form";
+
+// fetches customers and passes it to the <Form> component.
+export default async function CreateInvoice() {
+  const customers = await fetchCustomers();
+  return (
+    <main>
+      <h1>Create Invoice</h1>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
